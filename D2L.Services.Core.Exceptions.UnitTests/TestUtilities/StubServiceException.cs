544dad2ce83dd8254cb0f1ace14a315444afc494
Jacob Parker <Jacob.Parker@D2L.com>
2015-10-22 17:31:58 -0400

    Add unit tests

---
+++ D2L.Services.Core.Exceptions.UnitTests/TestUtilities/StubServiceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+namespace D2L.Services.Core.Exceptions.TestUtilities {
+	internal sealed class StubServiceException : ServiceException {
+		public StubServiceException(
+			ServiceErrorType errorType,
+			string message = null,
+			Exception innerException = null,
+			HttpStatusCode serviceStatusCode = default( HttpStatusCode )
+		) : base( errorType, message, innerException, serviceStatusCode ) {}
+
+		public override string ServiceName {
+			get { return "foobar"; }
+		}
+	}
+}
